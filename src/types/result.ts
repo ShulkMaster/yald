6518cbf0f09d2416c07960517895f6e234665fd0
shulkmaster <shulkmaster@gmail.com>
2024-02-25 00:25:48 -0600

    adding shell code for interactive REPL

---
+++ src/types/result.ts
@@ -0,0 +1,2 @@
+export type Result<T> = T | Error;
+export type Deferred<T> = Promise<T | Error>;
--- src/yald.ts
+++ src/yald.ts
@@ -1,2 +1,55 @@
 #!/usr/bin/env node
-console.log('Hello World');
+import { ExitCodes } from 'util/codes';
+import { promises } from 'readline';
+
+const args = process.argv.slice(2);
+let shouldExit = false;
+
+const exitCode = await main(args);
+process.exit(exitCode);
+
+async function main(args: string[]): Promise<ExitCodes> {
+  switch (args.length) {
+    case 0:
+      return await prompt();
+    case 1:
+      console.log(`Hello, ${args[0]}!`);
+      return ExitCodes.ExOk;
+    default:
+      console.error('Too many arguments');
+      return ExitCodes.ExUsage;
+  }
+}
+
+function sigIntHandler() {
+  if (shouldExit) {
+    process.stdout.write('\n');
+    process.exit(ExitCodes.ExUnavailable);
+  }
+  shouldExit = true;
+  process.stdout.write("type '.exit' to exit or press Ctrl+C again to force exit.\n");
+}
+
+async function prompt(): Promise<ExitCodes> {
+  const stdin = promises.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '>',
+  });
+
+  stdin.addListener('SIGINT', sigIntHandler);
+  stdin.write('Welcome to yald REPL!\n');
+  stdin.write('Type .exit to exit.\n');
+
+  for (;;) {
+    const line = await stdin.question('>');
+    if (line === '.exit') {
+      break;
+    }
+
+    console.log(line.split(' '));
+  }
+
+  stdin.close();
+  return ExitCodes.ExOk;
+}
